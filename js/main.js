fd058d3375e50fa3ad7171acf7a7f289fe077339
kadowning1 <kadowning1@gmail.com>
2021-09-10 15:11:26 -0400

    update before demo

--- js/main.js
+++ js/main.js
@@ -1,42 +1,69 @@
-setInterval (myFunction, 1000); /*function to make clock not refresh and update*/
+setInterval(myFunction, 1000); /*function to make clock not refresh and update*/
 
-function myFunction() {{
-  var currentDate = new Date(); /* creating the date*/
-  
-  var hour = currentDate.getHours(); /*uses local time according to MDN*/
-  var min = currentDate.getMinutes();
-  var sec = currentDate.getSeconds();
-  var ampm = "AM";
+function myFunction() { /*function to produce current time*/
+  {
+    var currentDate = new Date(); /* creating the date*/
+
+    var hour = currentDate.getHours(); /*uses local time according to MDN*/
+    var min = currentDate.getMinutes();
+    var sec = currentDate.getSeconds();
+    var ampm = "";
 
 
-  hour = changeTime(hour);
-  min = changeTime(min);
-  sec = changeTime(sec);
-  ampm = dayNight(ampm);
+    hour = changeTime(hour);
+    min = changeTime(min);
+    sec = changeTime(sec);
+    ampm = dayNight(ampm);
 
-  var digitalClock = hour + " : " + min + " : " + sec + " : " + ampm
+    var digitalClock = hour + " : " + min + " : " + sec + " : " + ampm
 
-  document.getElementById("time").innerHTML = digitalClock; /* adding to the html div.  this part took me the longest to figure out */
-}
-function changeTime(i) { /*adding zeros to all var if under 10*/
-  if (i < 10) {
-    return "0" + i;
-  } else {
-    return i
+    document.getElementById("time").innerHTML = digitalClock; /* adding to the html div.  this part took me the longest to figure out */
+  }
+
+  function changeTime(i) {
+    /*adding zeros to all var if under 10*/
+    if (i < 10) {
+      return "0" + i;
+    } else {
+      return i
+    }
+
+  }
+
+  function dayNight(hour) {
+    /*attempting to add am/pm*/
+    if (hour < 12) {
+      return "PM";
+    } else {
+      return "AM"
+    }
   }
 
 }
-function dayNight (hour) { /*attempting to add am/pm*/
-    if (hour < 12) {
-    return ampm;
-    }
-    else {
-        return "PM"
-    }
+
+function setAlarm() { /*this is the function holding me back*/
+  let hour = 07
+  let min = 38
+  let sec = 00
+  let ampm = "PM"
+  let digitalClock = (hour + min + sec + ampm)
+  console.log(digitalClock);
+
+  if (digitalClock === digitalClock) /*looking for something along these lines but need some sort of variable to set alarm*/
+    alert("Wake up!");
+  else
+    alert("Keep Sleeping");
 }
 
+function clearAlarm() { /*function to clear out time after setAlarm function is complete*/
+  clearInterval(setAlarm, 500);
+  alert("Alarm Cleared");
 }
+
+function snoozeAlarm() { /*wanted to add 5 minutes snooze but failed at other function so didnt get to it */
+  alert("No, Just Wake Up!");
+}
+
+
 setTimeout(myFunction, 3000);
 myFunction(); /*calling the function*/
-
-
--- psuedocode.md
+++ psuedocode.md
@@ -2,15 +2,23 @@ Work on HTML format
 Add minor CSS
 Set up JS (Date object and setInterval/setTimeout)
 
-Initiate current time into program
-(Hours:Minutes:Seconds)
-Create variables to show each
-AM/PM
+Initiate current time into program - check
+(Hours:Minutes:Seconds) - check
+Create variables to show each - check
+AM/PM - check
 
-Pick a time for it to go off:
+Pick a time for it to go off: - not accomplished
 
-Update without refreshing the page
+Update without refreshing the page - check
 
-Start alarm when time to 'prompt' alarm over
+Start alarm when time to 'prompt' alarm over - not accomplished
 
 Possible stretch goals
+Snooze button
+
+JS:
+
+Establish function for time - check
+Establish function for Set Alarm - fail
+Establish function for Clear Alarm - check
+Establish function for Snooze Alarm - fail
