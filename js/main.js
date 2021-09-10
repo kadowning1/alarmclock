02e517a4c17169decdd3e93161e734417e3483af
kadowning1 <kadowning1@gmail.com>
2021-09-09 21:46:31 -0400

    almost there

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-function myFunction() {
-    var currentDate = new Date();
-    var h = currentDate.getHours();
-    var m = currentDate.getMinutes();
-    var s = currentDate.getSeconds();
-    var timeClock = h + ":" m + ":" s;
-    var currentLocalTime = timeClock.toLocaleTimeString();
-    document.getElementsByID("time").innerHTML = h + ":" m + ":" s;
+setInterval (myFunction, 1000); /*function to make clock not refresh and update*/
+
+function myFunction() {{
+  var currentDate = new Date(); /* creating the date*/
+  
+  var hour = currentDate.getHours(); /*uses local time according to MDN*/
+  var min = currentDate.getMinutes();
+  var sec = currentDate.getSeconds();
+  var ampm = "AM";
+
+
+  hour = changeTime(hour);
+  min = changeTime(min);
+  sec = changeTime(sec);
+  ampm = dayNight(ampm);
+
+  var digitalClock = hour + " : " + min + " : " + sec + " : " + ampm
+
+  document.getElementById("time").innerHTML = digitalClock; /* adding to the html div.  this part took me the longest to figure out */
 }
-myFunction ();
+function changeTime(i) { /*adding zeros to all var if under 10*/
+  if (i < 10) {
+    return "0" + i;
+  } else {
+    return i
+  }
+
+}
+function dayNight (hour) { /*attempting to add am/pm*/
+    if (hour < 12) {
+    return ampm;
+    }
+    else {
+        return "PM"
+    }
+}
+
+}
+setTimeout(myFunction, 3000);
+myFunction(); /*calling the function*/
+
+
